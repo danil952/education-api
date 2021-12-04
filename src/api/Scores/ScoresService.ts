58e510b45b491603a9de24e908c532dbb0acfcd7
danil952 <4danik2000@gmail.com>
2021-12-04 15:16:45 +0300

    feat:check StudentsCourses list before accept mark

--- src/api/Scores/ScoresService.ts
+++ src/api/Scores/ScoresService.ts
@@ -4,10 +4,14 @@ import { JSObject } from '../../helpers/HelpersInterfaces'
 import HttpErrors from '../../helpers/ErrorsHTTP'
 import LessonsModel from '../Lessons/LessonsModel'
 import ScoresModel from './ScoresModel'
+import CoursesModel from '../Courses/CoursesModel'
+import StudentsCoursesModel from '../StudentsCourses/studentsCoursesModule'
 
 export default class ScoresService {
 	private static ScoresServiceModel = new BaseService(new ScoresModel())
 	private static LessonsServiceModel = new BaseService(new LessonsModel())
+	private static CoursesServiceModel = new BaseService(new CoursesModel())
+	private static StudentsCoursesServiceModel = new BaseService(new StudentsCoursesModel())
 
 	static async createNewScore(data: JSObject, userId: string) {
 		HelperClass.checkRequiredField('answer', data, 'string')
@@ -16,6 +20,13 @@ export default class ScoresService {
 
 		const lesson = await ScoresService.LessonsServiceModel.findById(data._lessonId)
 		if (!lesson) throw new HttpErrors('No such lesson', HttpErrors.types.BadRequest)
+
+		const recordExists = ScoresService.StudentsCoursesServiceModel.dataExists({
+			_coruseId: lesson._courseId,
+			_studentId: userId
+		})
+		if (!recordExists)
+			throw new HttpErrors('No such user in course list', HttpErrors.types.Forbidden)
 
 		const mark = lesson.keys[data.taskIndexNumber] == data.answer ? 1 : 0
 		const scoreData = {
